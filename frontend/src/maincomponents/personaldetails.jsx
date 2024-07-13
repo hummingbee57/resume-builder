61f2e25bf14cbe58f1ab4ffbd631a0646c84bbb4
Vasu Verma <vermavasu069@gmail.com>
2024-07-13 23:08:53 +0530

    Added routes and pages

--- frontend/src/maincomponents/personaldetails.jsx
+++ frontend/src/maincomponents/personaldetails.jsx
@@ -10,13 +10,6 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
 
 export function Personal_Details_Form() {
 return (
