13b8b9809dcd33728a83136f9d25b057e00f146c
hummingbee57 <bhasinmaya01@gmail.com>
2024-07-13 17:59:03 -0400

    set up CORS

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
-const express = require('express')
-const app = express()
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    })
+);
 
 app.get("/", (req, res) => {
     console.log("Here")
     res.send("Hi")
-})
+});
 
-app.listen(3000)
+app.listen(3000);
